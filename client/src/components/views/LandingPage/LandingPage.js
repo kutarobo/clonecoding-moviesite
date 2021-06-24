190dfc2ca933933611e7907bb6a138801ff0e870
kutarobo <kutarobo@naver.com>
2021-06-24 16:04:56 +0900

    feat: Add Landingpage
    랜딩페이지 main image view 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-
+import {API_KEY, API_URL, IMAGE_BASE_URL} from '../../Config';
+import MainImage from './Sections/MainImage'
 function LandingPage() {
+
+    //fixme react에서 제공하는 useState를 이용하여 state 지정 useState 공부하자.
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovies([response.results])
+                setMainMovieImage(response.results[0])
+            })
+    }, [])
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }} >
+            
+            {/* main image */}
+            { MainMovieImage && 
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+            
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movie by latest</h2>
+                <hr />
+                {/* movie grid cards */}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        </div>
     )
 }
 

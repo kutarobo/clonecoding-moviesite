3b81279e31d90fbbf351ef555b212ede11ed1a94
kutarobo <kutarobo@naver.com>
2021-06-28 12:36:07 +0900

    refactor: Fix shorthand style error
    background 단축스타일 경고 수정
    -단축스타일을 풀어서 고침, 단축과 비단축을 혼합하지마라

--- client/src/components/views/LandingPage/Sections/MainImage.js
+++ client/src/components/views/LandingPage/Sections/MainImage.js
@@ -5,11 +5,12 @@ function MainImage(props) {
   return (
     <div
       style={{
-        background: `linear-gradient(to bottom, rgba(0,0,0,0) 39%, rgba(0,0,0,0) 41%, rgba(0,0,0,0.65) 100%), url('${image}'), #1c1c1c`,
-        height: '500px',
+        backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 39%, rgba(0,0,0,0) 41%, rgba(0,0,0,0.65) 100%), url('${image}')`,
+        backgroundColor: '#1c1c1c',
         backgroundSize: '100%, cover',
         backgroundPosition: 'center, center',
         width: '100%',
+        height: '500px',
         position: 'relative',
       }}
     >
